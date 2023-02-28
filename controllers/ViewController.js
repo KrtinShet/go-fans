2ca431404f2cf9b55f3886b6dd7af26eae9c7343
Krtin Shet <krtinshet.dev@gmail.com>
2023-02-28 18:27:29 +0530

    login page, sign up page

--- controllers/ViewController.js
+++ controllers/ViewController.js
@@ -1,12 +1,23 @@
 const catchAsync = require('../utils/catchAsync');
 
-
 exports.getHomePage = catchAsync(async (req, res, next) => {
-    res.render('home.ejs', { title: "Home" })
-    // res.send('index')
-    // res.render('index.ejs')
+    res.render('home.ejs', { title: "Home", user: { name: "Krtin Shet" } })
 });
 
 exports.NotFound = catchAsync(async (req, res, next) => {
     res.render('404.ejs', { title: "404" })
+});
+exports.getLoginPage = catchAsync(async (req, res, next) => {
+    res.render('login.ejs', { title: "404" })
+});
+exports.getSignupPage = catchAsync(async (req, res, next) => {
+    res.render('signup.ejs', { title: "404" })
+});
+
+exports.postLoginPage = catchAsync(async (req, res, next) => {
+    console.log("post Login Page", req.body)
+});
+
+exports.postSignUpPage = catchAsync(async (req, res, next) => {
+    console.log("post Sign Up Page", req.body)
 });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
-const { getHomePage, NotFound } = require("./../controllers/ViewController")
+const { getHomePage, NotFound, getLoginPage, getSignupPage, postLoginPage, postSignUpPage, } = require("./../controllers/ViewController")
 
 router.get("/", getHomePage);
+
+
+router.route("/login").get(getLoginPage).post(postLoginPage);
+router.route("/signup").get(getSignupPage).post(postSignUpPage);
+
+
+
 
 
 router.get("*", NotFound);
